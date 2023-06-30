8878a5d3e0d1f8a4428537aaa1b26e7d48c6d947
Mohammed Rehan <109914073+morehan740@users.noreply.github.com>
2023-06-30 18:25:45 +0530

    Add files via upload

---
+++ chad.js
@@ -0,0 +1,5 @@
+function chadcal(){
+name1 = prompt("Enter your name");
+        var chadscore = Math.random() * 100;
+        chadscore=Math.floor(chadscore)+1;
+        alert ( name1 +  " your chad score is " +chadscore+ "%");}
